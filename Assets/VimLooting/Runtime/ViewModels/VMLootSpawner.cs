02f94ff0f2405bc5a24a49d55c05a06b25ceb1ba
niks <niksok13@gmail.com>
2023-05-23 20:10:00 +0300

    VMLootSpawner

---
+++ Assets/VimLooting/Runtime/ViewModels/VMLootSpawner.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using VimCore.Runtime.MVVM;
+using VimLooting.Runtime.Core;
+
+namespace VimLooting.Runtime.ViewModels
+{
+    
+    public class VMLootSpawner : AViewModel<Transform>
+    {
+        [Range(1, 10)]
+        public int amount;
+        public LootableDefinition loot;
+
+        public override void OnSignal()
+        {
+            for (int i = 0; i < amount; i++) 
+                SpawnLootable();
+        }
+
+        private void SpawnLootable()
+        {
+            var posFrom = transform.position;
+            var offset =  Random.insideUnitSphere;
+            offset.y = 0;
+            offset.Normalize();
+            var posTo = posFrom + offset;
+            loot.Spawn(posFrom, posTo);
+        }
+    }
+}

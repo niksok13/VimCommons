e2ca25ec74ce5ff952fc4ec93f41ff419b54cdd3
niks <niksok13@gmail.com>
2023-05-29 16:45:06 +0300

    Stackable conversion formula as SO

--- Assets/VimCommons/StackingBuildings/Runtime/StackableConverter/StackableConversionFormula.cs
+++ Assets/VimCommons/StackingBuildings/Runtime/StackableConverter/StackableConversionFormula.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 using VimCommons.Stacking.Runtime;
 
 namespace VimCommons.StackingBuildings.Runtime.StackableConverter
 {
-    [Serializable]
-    public struct StackableConversionFormula
+    [CreateAssetMenu]
+    public class StackableConversionFormula: ScriptableObject
     {
         public List<StackableConversionFormulaEntry> source;
         public StackableDefinition result;
